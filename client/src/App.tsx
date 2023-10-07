b6a101bbf36f3ecdd60898b23f58593ee65ff6e2
Maksim Rukomoynikov <max@apply4.com>
2023-10-07 21:45:40 +0100

    Generate typings for react apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from '@apollo/client';
+import { gql } from '../src/__generated__/';
+
+const GET_ARTICLES = gql(/* GraphQL */ `
+    query GetArticles {
+        articles {
+            id
+            title
+            description
+        }
+    }
+`);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loading, error, data } = useQuery(GET_ARTICLES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  if (!data) return <p>Not found</p>;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data.articles.map((article) => {
+        return <div key={article.id}>
+          <h2>{article.title}</h2>
+          <p>{article.description}</p>
+        </div>
+      })}
     </>
   )
 }
---
+++ codegen.ts
@@ -0,0 +1,24 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: './src/schema.gql',
+  documents: ['client/src/**/*.{ts,tsx}'],
+  generates: {
+    './client/src/__generated__/': {
+      preset: 'client',
+      plugins: [],
+      config: {
+        skipTypename: false,
+        withHooks: true,
+        withHOC: false,
+        withComponent: false,
+      },
+      presetConfig: {
+        gqlTagName: 'gql',
+      },
+    },
+  },
+  ignoreNoDocuments: true,
+};
+
+export default config;
